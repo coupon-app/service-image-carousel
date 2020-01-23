fa397fe34889fd72d913843b45d81a902d150786
Daniel Rowe <danielrowetech@gmail.com>
2020-01-23 15:38:51 -0800

    Display image Basic styling

--- client/src/components/ImageThumbnail.jsx
+++ client/src/components/ImageThumbnail.jsx
@@ -1,7 +1,20 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
+import styled from 'styled-components';
 
-const ImageThumbnail = () => (
-  <div />
+const Image = styled.img`
+  height: 57px;
+  width: 57px;
+  border-radius: 6px;
+  cursor: pointer;
+`;
+
+const ImageThumbnail = ({ src, onClick }) => (
+  <Image
+    src={src}
+    alt="Deal thumbnail"
+    onClick={onClick}
+  />
 );
 
 export default ImageThumbnail;
