9d9ae315f27aec74c8b2f6683746e607d3d411e1
Daniel Rowe <danielrowetech@gmail.com>
2020-01-23 17:06:10 -0800

    Basic implementation

---
+++ client/src/components/ImageViewer.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import styled from 'styled-components';
+
+const Image = styled.img`
+  border-radius: 6px;
+`;
+
+const ImageViewer = ({ imageUrl }) => (
+  <Image src={imageUrl} alt="Deal" />
+);
+
+export default ImageViewer;
