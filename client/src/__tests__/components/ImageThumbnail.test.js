2882350aaf9a380d7495b40f9eea5aeff205ebda
Daniel Rowe <danielrowetech@gmail.com>
2020-01-27 20:21:32 -0800

    Complete ImageThumbnail tests

--- client/src/__tests__/components/ImageThumbnail.test.js
+++ client/src/__tests__/components/ImageThumbnail.test.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer'
+import 'jest-styled-components'
 import ImageThumbnail from '../../components/ImageThumbnail';
 
-describe('<ImageThumbnail />', () => {
+describe('ImageThumbnail', () => {
   it('invoke callback on click', () => {
     const onClick = jest.fn();
-    const wrapper = shallow(<ImageThumbnail onClick={onClick}/>).dive();
+    const wrapper = shallow(<ImageThumbnail onClick={onClick} />).dive();
     wrapper.find('img').simulate('click');
     expect(onClick).toHaveBeenCalledTimes(1);
+  });
+
+  it('have 4px border when selected', () => {
+    const tree = renderer.create(<ImageThumbnail isSelected={true} />).toJSON();
+    expect(tree).toHaveStyleRule('border', /4px/);
   });
 });
