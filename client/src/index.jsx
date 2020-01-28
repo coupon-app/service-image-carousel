e474c903a1d9e9f84e7436fa890b474b9041d80f
Daniel Rowe <danielrowetech@gmail.com>
2020-01-28 14:09:45 -0800

    Rename App to Carousel

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-import App from './App';
+import App from './Carousel';
 
 ReactDOM.render(<App productId={1} />, document.getElementById('root'));
