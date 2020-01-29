025dbe87ff5e4e4ecfeb87f23b3c61e249ae69ad
Daniel Rowe <danielrowetech@gmail.com>
2020-01-28 18:13:38 -0800

    Add transition animation

--- client/src/components/ImageViewer.jsx
+++ client/src/components/ImageViewer.jsx
@@ -1,15 +1,30 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 
+const transitionDuration = 125 // in milliseconds
+
 const Image = styled.img`
   border-radius: 6px;
+  transition: opacity ${transitionDuration}ms ease-in-out;
+  ${(props) => (props.opacity === 1 ? 'opacity: 1' : 'opacity: 0')}
 `;
 
 
-const ImageViewer = ({ imageUrl }) => (
-  <Image src={imageUrl} alt="Deal" />
-);
+const ImageViewer = ({ imageUrl }) => {
+  const [displayUrl, setDisplaySrc] = useState(imageUrl);
+  const [opacity, setOpacity] = useState(1);
+
+  useEffect(() => {
+    setOpacity(0);
+    setTimeout(() => {
+      setDisplaySrc(imageUrl);
+      setOpacity(1);
+    }, transitionDuration);
+  }, [imageUrl]);
+
+  return (<Image src={displayUrl} opacity={opacity} alt="Deal" />);
+};
 
 export default ImageViewer;
