59bb70815cddd5a8e2dd468e8c60a3a3b35956a1
Daniel Rowe <danielrowetech@gmail.com>
2020-01-30 18:06:29 -0800

    Update tests for babel styled components

--- __tests__/components/ButtonsOverlay.test.js
+++ __tests__/components/ButtonsOverlay.test.js
@@ -8,16 +8,14 @@ describe('ButtonsOverlay', () => {
   it('invokes callback with -1 when left arrow is clicked', () => {
     const mockCallback = jest.fn();
     const wrapper = shallow(<ButtonsOverlay onClick={mockCallback} />);
-    const parentDiv = wrapper.findWhere((n) => n.name() === 'styled.div').first();
-    parentDiv.children().first().simulate('click');
+    wrapper.find('ButtonsOverlay__Button').first().simulate('click');
     expect(mockCallback).toHaveBeenCalledWith(-1);
   });
 
   it('invokes callback with 1 when right arrow is clicked', () => {
     const mockCallback = jest.fn();
     const wrapper = shallow(<ButtonsOverlay onClick={mockCallback} />);
-    const parentDiv = wrapper.findWhere((n) => n.name() === 'styled.div').first();
-    parentDiv.children().last().simulate('click');
+    wrapper.find('ButtonsOverlay__Button').last().simulate('click');
     expect(mockCallback).toHaveBeenCalledWith(1);
   });
 });
--- __tests__/components/ImageThumbnail.test.js
+++ __tests__/components/ImageThumbnail.test.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import renderer from 'react-test-renderer';
-import 'jest-styled-components';
 import ImageThumbnail from '../../client/src/components/ImageThumbnail';
 
 describe('ImageThumbnail', () => {
@@ -12,19 +10,15 @@ describe('ImageThumbnail', () => {
     expect(onClick).toHaveBeenCalledTimes(1);
   });
 
-  it('has a border of 0px when not selected', () => {
-    const tree = renderer.create(<ImageThumbnail isSelected={false} />).toJSON();
-    const borderDivs = tree.children.filter((element) => element.type === 'div');
-    borderDivs.forEach((borderDiv) => {
-      expect(borderDiv).toHaveStyleRule('border', expect.stringContaining('0px'));
-    });
+  it('sets borders to invisible when not selected', () => {
+    const wrapper = shallow(<ImageThumbnail isSelected={false} />);
+    expect(wrapper.find('ImageThumbnail__InnerBorder').props().visible).toBeFalsy();
+    expect(wrapper.find('ImageThumbnail__OuterBorder').props().visible).toBeFalsy();
   });
 
-  it('has border of more than 0 when selected', () => {
-    const tree = renderer.create(<ImageThumbnail isSelected />).toJSON();
-    const borderDivs = tree.children.filter((element) => element.type === 'div');
-    borderDivs.forEach((borderDiv) => {
-      expect(borderDiv).toHaveStyleRule('border', expect.not.stringContaining('0px'));
-    });
+  it('sets inner border to invisible when not selected', () => {
+    const wrapper = shallow(<ImageThumbnail isSelected />);
+    expect(wrapper.find('ImageThumbnail__InnerBorder').props().visible).toBeTruthy();
+    expect(wrapper.find('ImageThumbnail__OuterBorder').props().visible).toBeTruthy();
   });
 });
