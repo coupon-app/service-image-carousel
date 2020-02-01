932fdb0ed82ceabbac1bd5119e8faa81f1e55a2a
Daniel Rowe <danielrowetech@gmail.com>
2020-01-31 17:12:02 -0800

    Rename parent div id

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,4 +2,4 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import Carousel from './Carousel';
 
-ReactDOM.render(<Carousel productId={1} />, document.getElementById('root'));
+ReactDOM.render(<Carousel productId={1} />, document.getElementById('carousel'));
