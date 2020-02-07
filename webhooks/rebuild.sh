0bacdcc0537b3000c44e4908a65d20a7798d2185
Daniel Rowe <danielrowetech@gmail.com>
2020-02-06 20:35:57 -0800

    Stop and remove old containers and images

--- webhooks/rebuild.sh
+++ webhooks/rebuild.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
 git pull
+docker stop service-image-carousel_carousel_1
+docker rm service-image-carousel_carousel_1
+docker rmi service-image-carousel_carousel
 docker-compose up -d --build --force-recreate --no-deps carousel
